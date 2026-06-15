8376b160625f1ef323f064307d2a7ce67a64e469
Somya Dwivedi <somya.dwivedi@yahoo.com>
2026-06-15 12:22:35 -0400

    Initial Commit

---
+++ src/crossplatformlab/FileManager.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package crossplatformlab;
+
+/**
+ *
+ * @author Somya Dwivedi, 2026
+ */
+
+public class FileManager {
+    public static void main(String[] args) {
+        System.out.println("Hi I am a File Manager.");
+    }
+}
